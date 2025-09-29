4dc4538bc90f837725e2492cd7fa5100c1d53f47
zauTT <giozau1@gmail.com>
2025-09-29 15:39:07 +0400

    Initial Commit

---
+++ Finance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance
+//
+//  Created by Giorgi Zautashvili on 29.09.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
